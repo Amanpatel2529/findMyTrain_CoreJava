8a6292fd1faae7f54a93b0f1bc39a14b58db8742
Aman Patel <amanpatel.ajp@gmail.com>
2025-11-21 12:17:37 +0530

    Find My Train Proj - Core Java

---
+++ src/test/java/com/genie/SpringBoot/FindMyTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genie.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
